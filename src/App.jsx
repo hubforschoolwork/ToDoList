e04028e67519ebd875e0f39db1d2c687f08e95a5
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-02 15:30:33 -0700

    Edit navbar and app.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
 import React from 'react';
 import MyNavbar from './components/navbar';
-import Todo from './components/Todo';
-import Contacts from './components/Contacts'
+import Todo from './pages/Todo';
+import Contacts from './pages/Contacts';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
@@ -13,13 +13,13 @@ function App() {
 
   return (
     <Router>
-      <div className = "container primary">
+      <div className = "container">
         <div className="App">
           <MyNavbar />
-
+          <Switch>
           <Route path="/" exact component={Todo} />
-          <Route path="/" exact component={Contacts} />
-
+          <Route path="/contacts" exact component={Contacts} />
+          </Switch>
       
         </div>
       </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,41 +1,44 @@
-// src/components/Navbar.js
 import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
 
 const MyNavbar = () => {
   return (
-<div class="container">
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-<nav class="navbar bg-info border-bottom border-body" data-bs-theme="primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">TO DO LIST</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<div className="container">
+    <nav className="navbar navbar-expand-lg bg-info border-bottom border-body" data-bs-theme="primary">
+
+{/* <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar bg-info border-bottom border-body" data-bs-theme="primary"> */}
+  
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">TO DO LIST</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link" href="#">To Do</a>
+        <li className="nav-item">
+          <Link className="nav-link" to="/todo">To Do</Link>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contacts</a>
+        <li className="nav-item">
+          <Link className="nav-link" to="/contacts">Contacts</Link>
         </li>
 
       </ul>
 
-      <span class="navbar-text">
+      <span className="navbar-text">
         Extra!
       </span>
     </div>
   </div>
   </nav>
-</nav>
+
 </div>
 
 
