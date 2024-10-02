7b6ed0830c6c591e38468aa1aaf08e95afbf2627
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-02 11:23:38 -0700

    navbar - add formatting and color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import {HashRouter, Routes, Route} from 'react-router-dom'
-import ToDo from './pages/todo'
-import Contact from './pages/contacts'
+// import ToDo from './pages/todo'
+// import Contact from './pages/contacts'
 import React from 'react';
-import MyNavbar from './components/Navbar';
+import MyNavbar from './components/navbar';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,21 +4,39 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 const MyNavbar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">BrandName</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+<container>
+<nav class="navbar navbar-expand-lg bg-body-tertiary background-color: light blue">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar w/ text</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+
+      </ul>
+
+      <span class="navbar-text">
+        Navbar text with an inline element
+      </span>
+    </div>
+  </div>
+</nav>
+</container>
+
+
   );
 };
 
