689e1708dc101a0237ece78990eb10158a323d11
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-04 14:35:45 -0700

    Fix code for background image

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import MyNavbar from './components/navbar';
 import Todo from './pages/Todo';
 import Contacts from './pages/Contacts';
 import './App.css';
+import backgroundwavaes from './components/Backgroundbeach';
 
 
 function App() {
 
   return (
     <Router>
+
       <div className = "container-front">
         <div className="App">
+          <backgroundwaves />
           <MyNavbar />
           <Routes>
           <Route path="/" exact component={Todo} />
---
+++ src/components/Backgroundbeach.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import backgroundwaves from './App.css';
+
+const background-all = () => {
+    return (
+        <div className="background-all">
+
+        </div>
+    );
+
+
+}
