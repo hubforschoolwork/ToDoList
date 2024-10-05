29d39eb5cbc7ca1a0fd5ac0268d579aa1eb047fd
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-05 14:44:49 -0700

    Fix/edit css code

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,19 @@ import MyNavbar from './components/navbar';
 import Todo from './pages/Todo';
 import Contacts from './pages/Contacts';
 import './App.css';
-import BackgroundImageComponent from './components/BackgroundImageComponent';
+
 
 
 function App() {
 
   return (
     <Router>
-      <div style={{ position: 'relative', height: '100vh' }}>
-        <BackgroundImageComponent />
-        <div className="container-front" style={{ position: 'relative', zIndex: 1 }}>            
-          <div className="App">
+      <div className="App">
             <MyNavbar />
             <Routes>
               <Route path="/" element={<Todo/>} />
               <Route path="/contacts" element={<Contacts/>} />
             </Routes>
-          </div>
-        </div>
       </div>
     </Router>  
   );
