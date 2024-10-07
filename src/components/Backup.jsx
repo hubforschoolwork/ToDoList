47f78ff0026b875be701ddee6b0c277b66a0aca2
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-07 10:46:29 -0700

    contactform submission errors

---
+++ src/components/Backup.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+
+
+function ContactForm() {
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        comment: '',
+    });
+
+    const handleChange = (e) => {
+        const { firstname, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [firstname]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add submission logic here
+        if (!formData.name || !formData.email || !formData.message) {
+            alert('All fields are required!');
+            return;
+        }
+        // new code ends here
+        // console.log(formData);
+
+
+        //new code
+        fetch('your-api-endpoint', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            if (response.ok) {
+                setSubmissionStatus('Your message has been sent successfully.');
+                setFormData({ name: '', email: '', message: '' });
+            } else {
+                setSubmissionStatus('There was an error sending your message.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            setSubmissionStatus('There was an error sending your message.');
+        });
+    };
+
+//end new code
+
+
+
+
+    };
+
+    return (
+        <div className="contact-container">
+        <form onSubmit={handleSubmit} className="form-container">
+            <div>
+                <label>
+                First Name:
+                <input type="text" input class="form-control" name="firstname" value={formData.firstname} onChange={handleChange} />
+            </label>
+            </div>
+<br/>
+            <div>
+           <label>
+                Last Name:
+                <input type="text" input class="form-control" name="lastname" value={formData.lastname} onChange={handleChange} />
+            </label>
+            </div>
+<br/>
+
+            <div>
+            <label>
+                Email:
+                <input type="email" input class="form-control" name="email" value={formData.email} onChange={handleChange} />
+            </label>
+            </div>
+
+<br/>
+            <label>
+                Comments:
+                <textarea name="comments" input class="form-control" value={formData.comments} onChange={handleChange} />
+            </label>
+
+
+<br/>
+            <button type="submit" class="btn btn-info btn-outline-dark" button id="button1">Submit</button>
+            {/* new code */}
+            {submissionStatus && <p>{submissionStatus}</p>}
+        </form>
+        </div>
+    );
+}
+
+export default ContactForm;
+
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,67 +1,181 @@
 import React, { useState } from 'react';
 
-
 function ContactForm() {
-    const [formData, setFormData] = useState({
-        firstname: '',
-        lastname: '',
-        email: '',
-        comment: '',
-    });
+    const [formData, setFormData] = useState({ firstname: '', lastname: '', email: '', message: '' });
+    const [submissionStatus, setSubmissionStatus] = useState('');
 
-    const handleChange = (e) => {
-        const { firstname, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [firstname]: value,
-        }));
+    const handleChange = (event) => {
+        const { firstname, value } = event.target;
+        setFormData((prevData) => ({ ...prevData, [firstname]: value }));
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Add submission logic here
-        console.log(formData);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!formData.firstname || !formData.lastname || !formData.email || !formData.message) {
+            alert('All fields are required!');
+            return;
+        }
+
+        fetch('your-api-endpoint', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => {
+            if (response.ok) {
+                setSubmissionStatus('Your message has been sent successfully.');
+                setFormData({ firstname: '', lastname: '', email: '', message: '' });
+            } else {
+                setSubmissionStatus('There was an error sending your message.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            setSubmissionStatus('There was an error sending your message.');
+        });
     };
 
     return (
-        <div className="contact-container">
-        <form onSubmit={handleSubmit} className="form-container">
-            <div>
-                <label>
-                First Name:
-                <input type="text" input class="form-control" name="firstname" value={formData.firstname} onChange={handleChange} />
-            </label>
-            </div>
+        <form onSubmit={handleSubmit}>
+            
+                <label>First Name:
+                    <input type="text" name="firstname" value={formData.firstname} onChange={handleChange} />
+                </label>
+            
 <br/>
             <div>
-           <label>
-                Last Name:
-                <input type="text" input class="form-control" name="lastname" value={formData.lastname} onChange={handleChange} />
-            </label>
+                <label>Last Name:
+                    <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} />
+                </label>
             </div>
 <br/>
 
             <div>
-            <label>
-                Email:
-                <input type="email" input class="form-control" name="email" value={formData.email} onChange={handleChange} />
-            </label>
+                <label>Email:
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                </label>
             </div>
-
 <br/>
-            <label>
-                Comments:
-                <textarea name="comments" input class="form-control" value={formData.comments} onChange={handleChange} />
-            </label>
-
-
+            <div>
+                <label>Message:
+                    <textarea name="message" value={formData.message} onChange={handleChange} />
+                </label>
+            </div>
 <br/>
-            <button type="submit" class="btn btn-info btn-outline-dark" button id="button1">Submit</button>
 
+            <button type="submit">Submit</button>
+            {submissionStatus && <p>{submissionStatus}</p>}
         </form>
-        </div>
     );
 }
 
 export default ContactForm;
+
+
+
+
+
+// import React, { useState } from 'react';
+
+
+// function ContactForm() {
+//     const [formData, setFormData] = useState({
+//         firstname: '',
+//         lastname: '',
+//         email: '',
+//         comment: '',
+//     });
+
+//     const handleChange = (e) => {
+//         const { firstname, value } = e.target;
+//         setFormData((prevData) => ({
+//             ...prevData,
+//             [firstname]: value,
+//         }));
+//     };
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         // Add submission logic here
+//         if (!formData.name || !formData.email || !formData.message) {
+//             alert('All fields are required!');
+//             return;
+//         }
+//         // new code ends here
+//         // console.log(formData);
+
+
+//         //new code
+//         fetch('your-api-endpoint', {
+//             method: 'POST',
+//             body: JSON.stringify(formData),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         })
+//         .then(response => {
+//             if (response.ok) {
+//                 setSubmissionStatus('Your message has been sent successfully.');
+//                 setFormData({ name: '', email: '', message: '' });
+//             } else {
+//                 setSubmissionStatus('There was an error sending your message.');
+//             }
+//         })
+//         .catch((error) => {
+//             console.error('Error:', error);
+//             setSubmissionStatus('There was an error sending your message.');
+//         });
+//     };
+
+// //end new code
+
+
+
+
+//     };
+
+//     return (
+//         <div className="contact-container">
+//         <form onSubmit={handleSubmit} className="form-container">
+//             <div>
+//                 <label>
+//                 First Name:
+//                 <input type="text" input class="form-control" name="firstname" value={formData.firstname} onChange={handleChange} />
+//             </label>
+//             </div>
+// <br/>
+//             <div>
+//            <label>
+//                 Last Name:
+//                 <input type="text" input class="form-control" name="lastname" value={formData.lastname} onChange={handleChange} />
+//             </label>
+//             </div>
+// <br/>
+
+//             <div>
+//             <label>
+//                 Email:
+//                 <input type="email" input class="form-control" name="email" value={formData.email} onChange={handleChange} />
+//             </label>
+//             </div>
+
+// <br/>
+//             <label>
+//                 Comments:
+//                 <textarea name="comments" input class="form-control" value={formData.comments} onChange={handleChange} />
+//             </label>
+
+
+// <br/>
+//             <button type="submit" class="btn btn-info btn-outline-dark" button id="button1">Submit</button>
+//             {/* new code */}
+//             {submissionStatus && <p>{submissionStatus}</p>}
+//         </form>
+//         </div>
+//     );
+// }
+
+// export default ContactForm;
 
