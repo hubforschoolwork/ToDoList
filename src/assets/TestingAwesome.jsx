261cf9171525b790cd6c4b0c75fd0cdc01b3e0f4
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-10 16:41:12 -0700

    Add code to todo.jsx

--- src/assets/TestingAwesome.jsx
+++
@@ -1,8 +0,0 @@
-<FontAwesomeIcon icon="fa-solid fa-star" />
-
-<FontAwesomeIcon icon={faStar} />
-
-
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 // import TodoForm from '../components/TodoForm';
 import './Todo.css';
 
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faStar } from '@fortawesome/free-solid-svg-icons'
+
 
 
 
@@ -11,8 +10,9 @@ function Todo() {
     return (
         <div>
             <h3>Todo Form</h3>
-            <p><FontAwesomeIcon icon={faStar} /></p>
-            
+            <br/>
+            <br/>
+            <i class="fa-solid fa-star"></i>        
 
         </div>
     );
