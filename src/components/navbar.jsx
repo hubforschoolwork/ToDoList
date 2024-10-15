8786bd4bf9ab860f6f797211051e7337442d81b9
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-15 11:09:21 -0700

    edit navbar code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ const MyNavbar = () => {
     <nav className="navbar navbar-expand-lg bg-info border-bottom border-body" data-bs-theme="primary%">
 
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">TO DO LIST</a>
+    <a className="navbar-brand" href="#">MY LIST</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -16,16 +16,16 @@ const MyNavbar = () => {
     <div className="collapse navbar-collapse" id="navbarText">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-
-        <li className="nav-item">
-          <Link className="nav-link" to="/todo">To Do</Link>
+          <Link className="nav-link active" aria-current="page" to="/">To Do List</Link>
         </li>
 
         <li className="nav-item">
           <Link className="nav-link" to="/contacts">Contacts</Link>
         </li>
+
+        {/* <li className="nav-item">
+          <Link className="nav-link" to="/contacts">Contacts</Link>
+        </li> */}
 
       </ul>
 
