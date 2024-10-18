ecf19c0fa1b3d9269beac9c10756fbb0cd08eb22
hubforschoolwork <144491033+hubforschoolwork@users.noreply.github.com>
2024-10-18 15:21:22 -0700

    New config code (#6)
    * Code edits
    
    * edit scripts
    
    * edits to scripts
    
    * scripts
    
    * deploy edits
    
    * change config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
+  base: "/{todolist}/",
   plugins: [react()],
-})
+});
+
+
+
+
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
