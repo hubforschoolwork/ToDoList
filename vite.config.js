25b5c63e7161586e92fae74bf2e51f486c628096
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-10-19 16:29:37 -0700

    edits

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,10 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base: "/todolist/",
+  base: '/todolist/',
   plugins: [react()],
 });
 
 
 
 
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
